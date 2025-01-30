104f86daaa0d495d5301574663d94347d5ef9247
MiltonMullins <miltonmullins92@gmail.com>
2025-01-30 19:31:48 -0300

    add second solution to minmax

--- Hackerrank/Algorithms/mini_max_sum.go
+++ Hackerrank/Algorithms/mini_max_sum.go
@@ -21,6 +21,24 @@ func miniMaxSum(arr []int32) {
 	fmt.Printf("%v %v", min, max)
 }
 
+func miniMaxSumV2(arr []int32) {
+	var sum, min, max int32
+	for _, v := range arr {
+		sum += v
+	}
+	min = arr[0]
+	for i := 1; i < len(arr); i++ {
+		if min > arr[i] {
+			min = arr[i]
+		} else if arr[i] > max {
+			max = arr[i]
+		}
+	}
+	totalX := sum - min
+	totalY := sum - max
+	fmt.Println(totalY, totalX)
+}
+
 func main() {
 
 	ar := []int32{1,3,5,7,9}
