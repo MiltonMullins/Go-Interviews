8003bc79f4826dee5f16d2018d67bba48874d708
MiltonMullins <miltonmullins92@gmail.com>
2025-01-30 19:28:23 -0300

    add simple array sum hackerrank problem

---
+++ Hackerrank/Algorithms/simple_array_sum.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func simpleArraySum(ar []int32) int32 {
+	var sum int32 = 0
+
+	for i := 0; i < len(ar); i++ {
+		sum += ar[i]
+	}
+	return sum
+}
+
+func main() {
+
+	ar := []int32{1,2,3,4,5}
+	fmt.Println(simpleArraySum(ar))
+}
