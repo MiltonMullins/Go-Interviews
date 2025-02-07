561cfbaadb6d17159686e3545e5edfa14ffed212
MiltonMullins <miltonmullins92@gmail.com>
2025-02-07 20:39:38 -0300

    add version two for testing contains duplicate

--- LeetCode/002_contains_duplicate/contains_duplicate_test.go
+++ LeetCode/002_contains_duplicate/contains_duplicate_test.go
@@ -13,13 +13,33 @@ func TestContainsDuplicate(t *testing.T) {
 	funcs := []func([]int) bool{containsDuplicateBF, containsDuplicateSorting, containsDuplicateMap}
 
 	for _, testFunc := range funcs {
-		if res := testFunc(numsTrue); !reflect.DeepEqual(res, true){
+		if res := testFunc(numsTrue); !reflect.DeepEqual(res, true) {
 			t.Error("Fail Test!")
 		}
 
-		if res := testFunc(numsFalse); !reflect.DeepEqual(res, false){
+		if res := testFunc(numsFalse); !reflect.DeepEqual(res, false) {
 			fmt.Println(res)
 			t.Error("Fail Test!")
+		}
+	}
+
+}
+
+func TestContainsDuplicateV2(t *testing.T) {
+	testsCases := [][]int{
+		{1, 2, 3, 4, 5},
+		{1, 2, 3, 4, 5, 3},
+	}
+
+	expected := []bool{false, true}
+
+	funcs := []func([]int) bool{containsDuplicateBF, containsDuplicateSorting, containsDuplicateMap}
+
+	for index, nums := range testsCases {
+		for _, testFunc := range funcs {
+			if res := testFunc(nums); res != expected[index] {
+				t.Errorf("expected %t, got %t", expected[index], res)
+			}
 		}
 	}
 }
