1fc46daec1e4598eeb71f458cfc238de1a53615b
MiltonMullins <miltonmullins92@gmail.com>
2025-02-08 13:46:36 -0300

    add second solution to product except self problem.

--- LeetCode/007_product_of_array_except_self/product_except_self.go
+++ LeetCode/007_product_of_array_except_self/product_except_self.go
@@ -1,5 +1,5 @@
 /*
-Given an integer array nums, return an array answer such that answer[i] is equal to 
+Given an integer array nums, return an array answer such that answer[i] is equal to
 the product of all the elements of nums except nums[i].
 The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
 You must write an algorithm that runs in O(n) time and without using the division operation.
@@ -12,15 +12,16 @@ Example 2:
 Input: nums = [-1,1,0,-3,3]
 Output: [0,0,9,0,0]
 
+
 */
 
 package productexceptself
 
-//Brute forece Solution
-//Time Complexity: O(n^2)
-//Space Complexity: O(1)
+// Brute forece Solution
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
 func productExceptSelf(nums []int) []int {
-    products, curProd := []int{},1
+	products, curProd := []int{}, 1
 
 	for i := range nums {
 		curProd = 1
@@ -33,4 +34,34 @@ func productExceptSelf(nums []int) []int {
 	}
 
 	return products
+}
+// if the count of zeros are 0, which means all the elements in the nums array are non-zero 
+// and output of each element of the nums array in the ans array will be equal to product / nums[i].
+func productExceptSelf2(nums []int) []int {
+	n := len(nums)
+    ans := make([]int, n)
+    product := 1
+    zeros := 0
+
+    for _, num := range nums {
+        if num == 0 {
+            zeros++
+            continue
+        }
+        product *= num
+    }
+
+    if zeros == 1 {
+        for i, num := range nums {
+            if num == 0 {
+                ans[i] = product
+            }
+        }
+    } else if zeros == 0 {
+        for i, num := range nums {
+            ans[i] = product / num //without using the division operation. This isn't allowed
+        }
+    }
+
+    return ans
 }
--- LeetCode/007_product_of_array_except_self/product_except_self_test.go
+++ LeetCode/007_product_of_array_except_self/product_except_self_test.go
@@ -7,18 +7,22 @@ import (
 
 func TestProductExceptSelf(t *testing.T) {
 	testCases := [][]int{
-		{1,2,3,4},
-		{-1,1,0,-3,3},
+		{1, 2, 3, 4},
+		{-1, 1, 0, -3, 3},
 	}
 
-	expected := [][]int {
-		{24,12,8,6},
-		{0,0,9,0,0},
+	expected := [][]int{
+		{24, 12, 8, 6},
+		{0, 0, 9, 0, 0},
 	}
 
-	for index, nums := range testCases {
-		if res := productExceptSelf(nums); !reflect.DeepEqual(res,expected[index]) {
-			t.Errorf("\nexpected %v, got %v", expected[index], res)
+	funcs := []func([]int) []int{productExceptSelf2, productExceptSelf}
+
+	for _, testFunc := range funcs {
+		for index, nums := range testCases {
+			if res := testFunc(nums); !reflect.DeepEqual(res, expected[index]) {
+				t.Errorf("\nexpected %v, got %v", expected[index], res)
+			}
 		}
 	}
 }
