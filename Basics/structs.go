4eef4e9dd86a868f4288ceaec8d0d47b0c72f1fc
MiltonMullins <miltonmullins92@gmail.com>
2025-02-23 19:23:02 -0300

    add example of DIP implementation

--- Basics/structs.go
+++ Basics/structs.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"sort"
-	"slices"
 	"cmp"
+	"fmt"
+	"slices"
+	"sort"
 )
 
 type person struct {
@@ -56,21 +56,28 @@ func structsFun() {
 	}
 	fmt.Println(dog)
 
-    fmt.Println("SORTING ARRAY OF STRUCT")
-    audience := []person{
-        {"Alice", 35},
-        {"Bob", 45},
-        {"James", 25},
-    }
+	fmt.Println("SORTING ARRAY OF STRUCT")
+	audience := []person{
+		{"Alice", 35},
+		{"Bob", 45},
+		{"James", 25},
+	}
 	// Sorting slices of structs - using the sort interface.
-    sort.Sort(AgeFactor(audience))
-    fmt.Println(audience) 
+	sort.Sort(AgeFactor(audience))
+	fmt.Println(audience)
 
 	// Sorting slices of structs - using the SortFunc method from the slices package.
 	slices.SortFunc(audience,
-        func(a, b person) int {
-            return cmp.Compare(a.age, b.age)
-        })
+		func(a, b person) int {
+			return cmp.Compare(a.age, b.age)
+		})
+
+
+	//IMPLEMENTING DEPENDENCY INVERSION PRINCIPLE
+	Employee1 := Employee{
+		emailNotif: EmailNotification{},
+	}
+	Employee1.emailNotif.notify("Test")
 }
 
 type AgeFactor []person
@@ -85,3 +92,32 @@ func (a AgeFactor) Less(i, j int) bool {
 func (a AgeFactor) Swap(i, j int) {
 	a[i], a[j] = a[j], a[i]
 }
+
+
+//IMPLEMENTING DEPENDENCY INVERSION PRINCIPLE
+//The Dependency Inversion Principle states that high-level modules should not depend on low-level modules.
+//Both should depend on abstractions. Abstractions should not depend on details. Details should depend on abstractions.
+//In the example below, the Employee struct depends on the NotificationService interface.
+//The EmailNotification and SlackNotification structs implement the NotificationService interface.
+//The Employee struct can now use any type that implements the NotificationService interface.
+//This allows for easy swapping of the notification service without changing the Employee struct.
+type NotificationService interface {
+	notify(msg string)
+}
+
+type EmailNotification struct {}
+
+func (EmailNotification) notify(msg string) {
+	fmt.Println("Email notification: ", msg)
+}
+
+type SlackNotification struct {}
+
+func (SlackNotification) notify(msg string) {
+	fmt.Println("Slack notification: ", msg)
+}
+
+type Employee struct {
+	emailNotif NotificationService
+}
+
